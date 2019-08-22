fbb73afddfac78d569878880a650d54d1db1cbb3
李浩 <hao.li@blockc.pro>
2019-08-22 11:19:09 +0800

    包调整

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools  import setup
+
+setup(
+    name= 'canal-python',
+    version= '0.1',
+    packages = ['canal'],
+    author='haoyue',
+    author_email='haolee1990@qq.com',
+    license='MIT',
+    url='https://github.com/haozi3156666/canal-python'
+)
